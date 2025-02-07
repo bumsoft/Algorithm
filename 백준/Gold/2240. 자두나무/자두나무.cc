b902667ab4770b955d8cf3ecd4a6351400d8411d
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-02-08 01:13:36 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2388 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -2,68 +2,50 @@
 using namespace std;
 using ll = long long;
 
-//dp : 이동하거나 | 이동 안하거나
-// dp[1000][30][2]
-// 
-// dp[i][j][k] : i초에 j번 움직였고 k나무에 있는 경우!
-
 int arr[1001];
+int T, W;
 
-int dp[1001][31][3];
+int mem[1001][31][3];
 
-int main()
+int backTracking(int s, int w, int loc)
 {
+	if (w == W)
+	{
+		int r = 0;
+		for (int i = s; i <= T; i++)
+		{
+			if (arr[i] == loc) r++;
+		}
+		return r;
+	}
+	if (s > T) return 0; 
+
+	if (mem[s][w][loc] != -1) return mem[s][w][loc];
+	int add = 0;
+	if (arr[s] == loc) add++;
+
+	int cloc = loc == 1 ? 2 : 1;
+	return mem[s][w][loc] = max(backTracking(s + 1,w,loc) + add, backTracking(s + 1, w + 1, cloc) + add);
+}
+
+int main() {
+
 	ios_base::sync_with_stdio(0); cin.tie(0);
-	int T, W;
 	cin >> T >> W;
 	for (int i = 1; i <= T; i++)
 	{
 		cin >> arr[i];
 	}
-
-
-	for (int i = 1; i <= T; i++)
+	for (int i = 0; i <= T; i++)
 	{
-		for (int j = 0; j <= W; j++) //짝수라면, 1번나무 //홀수라면, 2번나무에 있음
+		for (int j = 0; j <= W; j++)
 		{
-			//t가 j보다 작은경우??
-			if (j == 0)
+			for (int k = 0; k <= 3; k++)
 			{
-				dp[i][j][1] = dp[i - 1][j][1];
-				if (arr[i] == 1) dp[i][j][1]++;
-				continue;
-			}
-
-			if (j % 2 == 0)//짝수
-			{
-				dp[i][j][1] = max(dp[i - 1][j - 1][2],dp[i - 1][j][1]);
-				if (arr[i] == 1) dp[i][j][1]++;
-			}
-			else
-			{
-				dp[i][j][2] = max(dp[i - 1][j - 1][1], dp[i - 1][j][2]);
-				if (arr[i] == 2) dp[i][j][2]++;
+				mem[i][j][k] = -1;
 			}
 		}
 	}
+	cout<<max(backTracking(1, 0, 1), backTracking(1, 1, 2));
 
-	int ans = 0;
-	for (int i = 0; i <= W; i++)
-	{
-
-		ans = max(ans, dp[T][i][1]);
-		ans = max(ans, dp[T][i][2]);
-	}
-	cout << ans;
-
-	//for (int i = 1; i <= T; i++)
-	//{
-	//	for (int j = 0; j <= W; j++) //짝수라면, 1번나무 //홀수라면, 2번나무에 있음
-	//	{
-	//		cout << "dp[" << i << "][" << j << "][1]=" << dp[i][j][1] << ' ';
-	//		cout << "dp[" << i << "][" << j << "][2]=" << dp[i][j][2] << '\n';
-	//	}
-	//}
-
-	return 0;
 }
