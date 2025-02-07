a7843e13875aff85cdc8fdb555ef2f156aa3c5f0
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-02-07 18:46:27 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.cc
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -2,36 +2,33 @@
 using namespace std;
 using ll = long long;
 
-//O(N^2) LIS
+//O(NlogN) LIS
 
-int arr[1'001];
-int dp[1'001]; //dp[i]: arr[i]를 가장 큰 수로 하는 수열의 길이
+vector<int> lis;
 
 int main() {
 
 	ios_base::sync_with_stdio(0); cin.tie(0);
 	int n;
 	cin >> n;
+	int t;
 	for (int i = 1; i <= n; i++)
 	{
-		cin >> arr[i];
-	}
+		cin >> t;
 
-	for (int i = 1; i <= n; i++)
-	{
-		dp[i] = 1;
-		for (int j = 1; j < i; j++)
+		auto pos = lower_bound(lis.begin(), lis.end(), t);
+
+		if (pos == lis.end())
 		{
-			if (arr[j] < arr[i])
-			{
-				dp[i] = max(dp[i], dp[j] + 1);
-			}
+			lis.push_back(t);
+		}
+		else
+		{
+			*pos = t;
+			//또는
+			//lis[pos-lis.begin()] = t;
 		}
 	}
-	int maxx = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		maxx = max(maxx, dp[i]);
-	}
-	cout << maxx;
+	cout << lis.size();
+
 }
