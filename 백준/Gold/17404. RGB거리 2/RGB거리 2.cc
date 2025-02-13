c796d4be07ca19a312e986bc0b70737186bb4cd0
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-02-14 03:11:41 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -1,65 +1,67 @@
 #include <bits/stdc++.h>
-#define INF INT_MAX
 using namespace std;
 using ll = long long;
 
 int N;
-int R[1001];
-int G[1001];
-int B[1001];
+int cost[1001][3];
 
-ll dp[1001][3];
-ll ans;
-int main()
-{
-	ios_base::sync_with_stdio(false); cin.tie(0);
+int dp[1001][3];
+
+int main() {
+
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
-		cin >> R[i] >> G[i] >> B[i];
+		cin >> cost[i][0];
+		cin >> cost[i][1];
+		cin >> cost[i][2];
 	}
-	//0번집을 고정시키고 dp
-	//빨강
-	dp[0][0] = R[0];
-	dp[0][1] = INF;
-	dp[0][2] = INF;
-	for (int i = 1; i < N; i++)
+
+	dp[0][0] = cost[0][0];
+	dp[0][1] = cost[0][1];
+	dp[0][2] = cost[0][2];
+
+	int ans = INT_MAX;
+	//0번이 0
+	dp[1][0] = INT_MAX;
+	dp[1][1] = dp[0][0] + cost[1][1];
+	dp[1][2] = dp[0][0] + cost[1][2];
+	for (int i = 2; i < N; i++)
 	{
-		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + R[i];
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
 
-		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + G[i];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
 
-		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + B[i];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
 	}
-	ans = min(dp[N - 1][1], dp[N - 1][2]);
-
-	//초록
-	dp[0][0] = INF;
-	dp[0][1] = G[0];
-	dp[0][2] = INF;
-	for (int i = 1; i < N; i++)
+	ans = min(ans, min(dp[N-1][1], dp[N-1][2]));
+	//0번이 1
+	dp[1][0] = dp[0][1] + cost[1][0];
+	dp[1][1] = INT_MAX;
+	dp[1][2] = dp[0][1] + cost[1][2];
+	for (int i = 2; i < N; i++)
 	{
-		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + R[i];
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
 
-		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + G[i];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
 
-		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + B[i];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
 	}
-	ans = min({ ans, dp[N - 1][0], dp[N - 1][2] });
-
-	//파랑
-	dp[0][0] = INF;
-	dp[0][1] = INF;
-	dp[0][2] = B[0];
-	for (int i = 1; i < N; i++)
+	ans = min(ans, min(dp[N - 1][0], dp[N - 1][2]));
+	//0번이 2
+	dp[1][0] = dp[0][2] + cost[1][0];
+	dp[1][1] = dp[0][2] + cost[1][1];
+	dp[1][2] = INT_MAX;
+	for (int i = 2; i < N; i++)
 	{
-		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + R[i];
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
 
-		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + G[i];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
 
-		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + B[i];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
 	}
-	ans = min({ ans, dp[N - 1][0], dp[N - 1][1] });
+	ans = min(ans, min(dp[N - 1][1], dp[N - 1][0]));
 	cout << ans;
-	return 0;
 }
