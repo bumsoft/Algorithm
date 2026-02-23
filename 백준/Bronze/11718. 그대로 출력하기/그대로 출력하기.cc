5601fd61a9f19045c00a8827d0014f1724da1a04
신진범 <140528690+bumsoft@users.noreply.github.com>
2026-02-23 18:03:00 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	char c;
+	while (cin.get(c))
+	{
+		cout << c;
+	}
+
+	return 0;
+}
