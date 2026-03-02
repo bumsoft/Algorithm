f026bf45bcc20fdfdb2a0a732edcb1f4840d3f9b
신진범 <140528690+bumsoft@users.noreply.github.com>
2026-03-03 00:01:57 +0900

    [level 3] Title: 이중우선순위큐, Time: 10.66 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -2,116 +2,39 @@
 
 using namespace std;
 
-// 정답 벡터가 <int>니 int로 가정하고 풀어보자.
-// 최소PQ, 최대PQ
-// map<int, int> m; 로 각 숫자의 잔여 개수 카운트
-
-vector<int> solution(vector<string> operations) {
+vector<int> solution(vector<string> o) {
     vector<int> answer;
     
-    priority_queue<int> highPQ;
-    priority_queue<int> lowPQ;
-    map<int,int> m;
-    int total = 0;
-    for(int i=0;i<operations.size();i++)
+    multiset<int> ms;
+    
+    for(int i=0;i<o.size();i++)
     {
-        if(operations[i][0] == 'I') // 삽입
+        if(o[i][0] == 'I')
         {
-            int num = stoi(operations[i].substr(2, operations[i].size()-2));
-            highPQ.push(num);
-            lowPQ.push(-num);
-            m[num]++;
-            total++;
+            int num = stoi(o[i].substr(2, o[i].size()-2));
+            ms.insert(num);
         }
-        else if(operations[i][2] == '1') // 최댓값 삭제
+        else if(o[i][2] == '1')
         {
-            if(total == 0) // 비어있다면
-            {
-                continue;
-            }
-            while(1)
-            {
-                int num = highPQ.top();
-                highPQ.pop();
-                
-                if(m[num] == 0) // 이미 없다면.
-                {
-                    continue;
-                }
-                else
-                {
-                    m[num]--;
-                    total--;
-                    break;
-                }
-            }
+            if(ms.size()==0) continue;
+            ms.erase(prev(ms.end()));
         }
-        else // 최솟값 삭제
+        else
         {
-            if(total == 0)
-            {
-                continue;
-            }
-            while(1)
-            {
-                int num = -lowPQ.top();
-                lowPQ.pop();
-                if(m[num] == 0) continue;
-                else
-                {
-                    m[num]--;
-                    total--;
-                    break;
-                }
-            }
+            if(ms.size()==0) continue;
+            ms.erase(ms.begin());
         }
     }
-    if(total == 0)
+    if(ms.size()==0)
     {
         answer.push_back(0);
         answer.push_back(0);
-        return answer;
     }
     else
     {
-        // 최대값
-        bool high = 0;
-        while(1)
-        {
-            if(highPQ.empty()) break;
-            int num = highPQ.top();
-            highPQ.pop();
-            if(m[num] != 0)
-            {
-                high = 1;
-                answer.push_back(num);
-                break;
-            }
-        }
-        
-        // 최소값
-        while(1)
-        {
-            if(lowPQ.empty()) break;
-            int num = -lowPQ.top();
-            lowPQ.pop();
-            if(m[num] != 0)
-            {
-                if(!high)
-                {
-                    answer.push_back(num);
-                    answer.push_back(num);
-                    break;
-                }
-                else
-                {
-                    answer.push_back(num);
-                    break;
-                }
-            }
-        }
+        answer.push_back(*(prev(ms.end())));
+        answer.push_back(*(ms.begin()));
     }
-    
     
     return answer;
 }
