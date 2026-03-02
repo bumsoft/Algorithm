76276dc60a22e406d71ec9e026bf254d0c260251
신진범 <140528690+bumsoft@users.noreply.github.com>
2026-03-03 01:41:46 +0900

    [level 2] Title: 튜플, Time: 10.25 ms, Memory: 6.28 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 1 + 2 + 3 + ... + n + 2*n;
+
+// 최대 1500개 정도의 집합이 들어있음 //N^2도 충분히 돈다.
+
+// 모든 집합을 집합의 원소 개수 순으로 정렬시켜야함
+// => vector<pair<int,string>>; //벡터 크기는 최대 1500
+
+bool vis[100'001];
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    
+    vector<pair<int,string>> v;
+    s = s.substr(1,s.size()-2);
+    // "},{" 기준으로 분리한다.
+    int start = 1;
+    int cnt = 1;
+    for(int i=1;i<s.size();i++)
+    {
+        if(s[i]=='}')
+        {
+            v.push_back({cnt,s.substr(start, i-start)});
+            i += 2;
+            start = i+1;
+            cnt = 1;
+        }
+        else if(s[i] == ',') cnt++;
+    }
+    sort(v.begin(), v.end());
+    for(int i=0;i<v.size();i++)
+    {
+        stringstream ss(v[i].second);
+        string x;
+        while(getline(ss, x, ','))
+        {
+            int num = stoi(x);
+            if(vis[num]) continue;
+            vis[num] = 1;
+            answer.push_back(num);
+        }
+    }
+    return answer;
+}
