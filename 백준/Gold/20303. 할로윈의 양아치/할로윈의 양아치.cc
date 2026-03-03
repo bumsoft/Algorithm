c860e739ac94ce37828caa189a665d8e354c4165
신진범 <140528690+bumsoft@users.noreply.github.com>
2026-03-03 16:50:06 +0900

    [Gold II] Title: 할로윈의 양아치, Time: 80 ms, Memory: 4632 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.cc
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.cc
@@ -1,73 +1,83 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 using ll = long long;
 
-int N, M, K;
+int root[30'001];
+
 int candy[30'001];
-vector<int> fr[30'001];
-bool vis[30'001];
-vector<pair<int, int>> save; //<candy, kidsm>
-int sum;
 
-int dp[30001][3001];
-
-bool vis2[30'001];
-int dfs(int s)
+int find_parent(int x)
 {
-	int get = candy[s];
-	sum++;
-	for (int i = 0; i < fr[s].size(); i++)
-	{
-		if (vis[fr[s][i]]) continue;
-		vis[fr[s][i]] = 1;
-		get += dfs(fr[s][i]);
-	}
-	return get;
+	if (x == root[x]) return x;
+	return root[x] = find_parent(root[x]);
 }
 
+int union_parent(int a, int b)
+{
+	a = find_parent(a);
+	b = find_parent(b);
+	if (a == b) return a;
+
+	if (a < b) return root[b] = a;
+	else return root[a] = b;
+}
 
 int main()
 {
-	ios_base::sync_with_stdio(0); cin.tie(0);
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N, M, K;
 	cin >> N >> M >> K;
+
+	for (int i = 1; i <= N; i++) root[i] = i;
+
+	// 사탕 수 입력
 	for (int i = 1; i <= N; i++)
 	{
 		cin >> candy[i];
 	}
+
+	// 친구관계 입력
 	int a, b;
 	for (int i = 0; i < M; i++)
 	{
 		cin >> a >> b;
-		fr[a].push_back(b);
-		fr[b].push_back(a);
+		union_parent(a, b);
 	}
-	save.push_back({ 0,0 });
-	//dfs
+	map<int, pair<int,int>> m; //루트번호, <사람수,사탕수>
 	for (int i = 1; i <= N; i++)
 	{
-		if (vis[i]) continue;
-		sum = 0;
-		vis[i] = 1;
-		int get = dfs(i);
-		save.push_back({ get, sum });
+		int r = find_parent(i);
+		m[r].first++;
+		m[r].second += candy[i];
 	}
-
-	//dp[i][j]: i번 그룹 선택ox, [j]명 울었을 때 최대값;
-	for (int i = 1; i < save.size(); i++)
+	vector<pair<int, int>> v; //<사람수, 사탕수>
+	for (auto it = m.begin(); it != m.end(); it++)
 	{
-		for (int j = 1; j < K; j++)
+		v.push_back(it->second);
+		//cout << "사람수: " << it->second.first << " 사탕수: " << it->second.second << '\n';
+	}
+	// v로 배낭문제 풀기
+	vector<int> dp(K+1);
+	
+	for (int i = 0; i <v.size(); i++)
+	{
+		int kids = v[i].first;
+		int candys = v[i].second;
+		for (int j = K; j >= 0; j--)
 		{
-			if (save[i].second > j) //i그룹 선택 못하는 경우
-			{
-				dp[i][j] = dp[i - 1][j];
-			}
-			else // 선택 가능한 경우, 1. 선택 안하는 경우와 2. 선택 하는 경우 중 선택
-			{
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - save[i].second] + save[i].first);
-			}
+			if (j - kids < 0) continue;
+			dp[j] = max(dp[j], dp[j - kids] + candys);
 		}
 	}
-	cout << dp[save.size() - 1][K - 1];
+	int ans = 0;
+	for (int i = 0; i < K; i++)
+	{
+		ans = max(ans, dp[i]);
+	}
+	cout << ans;
+
 
 	return 0;
 }
