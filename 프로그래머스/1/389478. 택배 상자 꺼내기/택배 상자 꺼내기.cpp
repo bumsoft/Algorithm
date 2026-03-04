513501f01d17b6ee199f526e2f6f72015dbb18d0
신진범 <140528690+bumsoft@users.noreply.github.com>
2026-03-04 19:29:56 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 4.43 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -2,74 +2,65 @@
 
 using namespace std;
 
+int arr[100][100];
+
 int solution(int n, int w, int num) {
-    int answer = 1;
+    int answer = 0;
     
-    if(w == 1)
+    int now = 1;
+    bool right = 1;
+    int w_cnt = 0;
+    int i=0;
+    int j=0;
+    while(1)
     {
-        return n-num+1;
-    }
-    
-    int floor = (num-1)/w; // num이 몇층에 있는지. 0층~
-    int max_floor = (n-1)/w; // 전체 몇층인지
-    cout<< floor << ' '<<max_floor<<'\n';
-    
-    if(floor == max_floor) return 1; //맨 위층이면.
-    // 최상층과 현재층 사이의 완전한 층 개수
-    answer += max_floor - floor - 1;
-    
-    // 짝수층이면 ->
-    // 홀수층이면 <-
-    int order = n%w; // 좌측에서 order 번째에 위치함
-    if(max_floor%2!=0) //홀수층
-    {
-        if(order != 0)
-            order = w-order + 1;
-    }
-    else if(order == 0) order = w;
-    
-    // num이 좌측에서 몇번째인지
-    int num_order = num%w;
-    if(floor%2!=0) //홀수층
-    {
-        if(num_order != 0)
-            num_order = w-num_order + 1;
-    }
-    else
-    {
-        if(num_order == 0)
+        if(now > n) break;
+        if(w_cnt<w)
         {
-            num_order = w;
+            if(right)
+            {
+                arr[i][j] =now;
+                w_cnt++;
+                j++;
+            }
+            else
+            {
+                arr[i][j] = now;
+                w_cnt++;
+                j--;
+            }
+        }
+        else
+        {
+            right = !right;
+            i++;
+            w_cnt=0;
+            if(right) j = 0;
+            else j = w-1;
+            
+            continue;
+        }
+        
+        now++;
+    }
+    int ii,jj;
+    for(int i=0; ;i++)
+    {
+        for(int j=0;j<w;j++)
+        {
+            if(arr[i][j]==num)
+            {
+                int answer = 1;
+                while(true)
+                {
+                    if(arr[++i][j] != 0)
+                        answer++;
+                    else return answer;
+                }
+            }
+                
         }
     }
-    if(num_order == order) return answer+1;
-    cout<<"DD"<<num_order<<' '<<order<<'\n';
-    
-    //둘 다 왼쪽방향에서 오는경우
-    if(floor%2==0 && max_floor%2==0)
-    {
-        if(num_order > order) return answer;
-        return answer+1;
-    }
-    //둘 다 오른방향에서 오는 경우
-    if(floor%2==1 && max_floor%2==1)
-    {
-        if(num_order < order) return answer;
-        return answer + 1;
-    }
-    //num 왼
-    if(floor%2==0)
-    {
-        if(num_order < order) return answer;
-        return answer+1;
-    }
-    //num 오
-    else
-    {
-        if(num_order > order) return answer;
-        return answer+1;
-    }
-    
-    
-    return answer;
+
+    return 0;
 }
