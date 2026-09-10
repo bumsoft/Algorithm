8a51fc38da7c2f5a7794fed3b645098a0d8c3238
신진범 <140528690+bumsoft@users.noreply.github.com>
2026-09-11 03:40:08 +0900

    [level 3] Title: 등굣길, Time: 0.10 ms, Memory: 4.65 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -1,53 +1,47 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
-using ll = long long;
 
-bool board[101][101];
-ll dp[101][101];
+long long dp[101][101];
 
 int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
     
     for(int i=0;i<puddles.size();i++)
     {
-        int mm = puddles[i][0];
-        int nn = puddles[i][1];
-        board[nn][mm] = 1;
+        dp[puddles[i][0]][puddles[i][1]] = -1;
     }
     
-    for(int i=1;i<=n;i++)
+    
+    for(int i=1;i<=m;i++)
     {
-        for(int j=1;j<=m;j++)
+        for(int j=1;j<=n;j++)
         {
-            if(board[i][j]) continue;
-            if(i == 1)
+            if(i==1 && j==1)
             {
-                if(j==1)
-                {
-                    dp[i][j] = 1;
-                }
-                else
-                {
-                    dp[i][j] = dp[i][j-1]%1'000'000'007;
-                }
+                dp[1][1] = 1;
+                continue;
             }
-            else
+            //물 확인
+            if(dp[i][j] == -1) continue;
+            // 맨 처음 예외처리
+            if(j==1) 
             {
-                if(j==1)
-                {
-                    dp[i][j] = dp[i-1][j]%1'000'000'007;
-                }
-                else
-                {
-                    ll up = dp[i-1][j];
-                    ll left = dp[i][j-1];
-                    if(!up && !left) dp[i][j] = 0;
-                    else if(!up) dp[i][j] = left;
-                    else if(!left) dp[i][j]= up;
-                    else dp[i][j] = (up+left)%1'000'000'007;
-                }
+                dp[i][j] = dp[i-1][j];
+                dp[i][j] %= 1'000'000'007;
+                continue;
             }
+            if(i==1) 
+            {
+                dp[i][j] = dp[i][j-1];
+                dp[i][j] %= 1'000'000'007;
+                continue;
+            }
+            dp[i][j] = ((dp[i-1][j]==-1)?0:dp[i-1][j]) + ((dp[i][j-1]==-1)?0:dp[i][j-1]);
+            dp[i][j] %= 1'000'000'007;
         }
     }
-    return dp[n][m];
+    
+    return dp[m][n]%1'000'000'007;
 }
