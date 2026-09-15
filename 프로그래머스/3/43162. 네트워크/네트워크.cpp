ca411b88238b8b93a5e02fbfc531bd9c6bc30154
신진범 <140528690+bumsoft@users.noreply.github.com>
2026-09-16 01:47:55 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,8 +1,11 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
 bool vis[201];
+queue<int> Q;
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
@@ -10,25 +13,26 @@ int solution(int n, vector<vector<int>> computers) {
     for(int i=0;i<n;i++)
     {
         if(vis[i]) continue;
-        answer++;
-        cout<<i<<'\n';
-        vis[i] = 1;
-        queue<int> Q;
         Q.push(i);
+        vis[i] = 1;
         while(!Q.empty())
         {
             int now = Q.front();
             Q.pop();
-            for(int t=0;t<computers[now].size();t++)
+            for(int j=0;j<n;j++)
             {
-                if(computers[now][t] != 1) continue;
-                if(vis[t]) continue;
-                vis[t] =1;
-                Q.push(t);
+                if(j == now) continue;
+                if(computers[now][j] && !vis[j])
+                {
+                    Q.push(j);
+                    vis[j] = 1;
+                }
             }
         }
-        
+        answer++;
     }
+
+
     
     return answer;
 }
