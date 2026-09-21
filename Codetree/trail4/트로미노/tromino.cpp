5e805f597b4e818f7a2a7109bd885f1f400f283f
신진범 <140528690+bumsoft@users.noreply.github.com>
2026-09-21 19:18:36 +0900

    [Passed] 트로미노 (2ms, 0MB)

--- Codetree/trail4/트로미노/tromino.cpp
+++ Codetree/trail4/트로미노/tromino.cpp
@@ -39,12 +39,12 @@ int main() {
     // [i][j]가 각 블럭의 중심이라고 가정하고, [i][j]에서 가능한 경우 4 + 2가지를 모두 계산해본다.
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<m;j++)
         {
             for(int ttt=0;ttt<6;ttt++) temp[ttt]=0;
             for(int a=0;a<3;a++)
             {
-                if(i+si1[a]>=0 && i+si1[a]<n && j+sj1[a] >=0 && j+sj1[a] <n)
+                if(i+si1[a]>=0 && i+si1[a]<n && j+sj1[a] >=0 && j+sj1[a] <m)
                 {
                     if(temp[0]!=-1) temp[0] += grid[i+si1[a]][j+sj1[a]];
                 }
@@ -53,7 +53,7 @@ int main() {
                     temp[0] = -1;
                 }
 
-                if(i+si2[a]>=0 && i+si2[a]<n && j+sj2[a] >=0 && j+sj2[a] <n)
+                if(i+si2[a]>=0 && i+si2[a]<n && j+sj2[a] >=0 && j+sj2[a] <m)
                 {
                     if(temp[1]!=-1) temp[1] += grid[i+si2[a]][j+sj2[a]];
                 }
@@ -62,7 +62,7 @@ int main() {
                     temp[1] = -1;
                 }
 
-                if(i+si3[a]>=0 && i+si3[a]<n && j+sj3[a] >=0 && j+sj3[a] <n)
+                if(i+si3[a]>=0 && i+si3[a]<n && j+sj3[a] >=0 && j+sj3[a] <m)
                 {
                     if(temp[2]!=-1) temp[2] += grid[i+si3[a]][j+sj3[a]];
                 }
@@ -71,7 +71,7 @@ int main() {
                     temp[2] = -1;
                 }
 
-                if(i+si4[a]>=0 && i+si4[a]<n && j+sj4[a] >=0 && j+sj4[a] <n)
+                if(i+si4[a]>=0 && i+si4[a]<n && j+sj4[a] >=0 && j+sj4[a] <m)
                 {
                     if(temp[3]!=-1) temp[3] += grid[i+si4[a]][j+sj4[a]];
                 }
@@ -80,7 +80,7 @@ int main() {
                     temp[3] = -1;
                 }
 
-                if(i+ti1[a]>=0 && i+ti1[a]<n && j+tj1[a] >=0 && j+tj1[a] <n)
+                if(i+ti1[a]>=0 && i+ti1[a]<n && j+tj1[a] >=0 && j+tj1[a] <m)
                 {
                     if(temp[4]!=-1) temp[4] += grid[i+ti1[a]][j+tj1[a]];
                 }
@@ -89,7 +89,7 @@ int main() {
                     temp[4] = -1;
                 }
 
-                if(i+ti2[a]>=0 && i+ti2[a]<n && j+tj2[a] >=0 && j+tj2[a] <n)
+                if(i+ti2[a]>=0 && i+ti2[a]<n && j+tj2[a] >=0 && j+tj2[a] <m)
                 {
                     if(temp[5]!=-1) temp[5] += grid[i+ti2[a]][j+tj2[a]];
                 }
