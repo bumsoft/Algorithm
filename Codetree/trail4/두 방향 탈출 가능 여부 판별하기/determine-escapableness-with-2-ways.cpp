8dca9b72b81c79fa5ef69fa274f7b4d56455cf38
신진범 <140528690+bumsoft@users.noreply.github.com>
2026-09-23 18:27:14 +0900

    [Time Limit Exceed] 두 방향 탈출 가능 여부 판별하기 (578ms, 0MB)

---
+++ Codetree/trail4/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int grid[100][100];
+
+int dx[2] = {0,1};
+int dy[2] = {1,0};
+
+bool ans;
+void dfs(int r, int c)
+{
+    if(ans) return;
+    for(int i=0;i<2;i++)
+    {
+        if(r+dx[i] >=N || c+dy[i] >=M) continue;
+        if(r+dx[i]==N-1 && c+dy[i]==M-1)
+        {
+            ans=1;
+            return;
+        }
+        if(grid[r+dx[i]][c+dy[i]] == 0) continue;
+        dfs(r+dx[i], c+dy[i]);
+    }
+}
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+    dfs(0,0);
+    cout<< (ans) ? 1:0;
+
+    return 0;
+}
